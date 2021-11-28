efc9b7bd50498c3fccf048b42111e3a1cb13596a
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-11-28 21:21:36 +0000

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { GrGithub, GrLinkedin } from "react-icons/gr"
+import { 
+    Container,
+    Row,
+    Col} from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <Container fluid
+            className="background-color">
+            <footer>
+                <Row className="mx-auto my-auto">
+                    <Col lg={12}
+                        id="footer-icons"
+                        className="d-flex justify-content-center">
+                            <a 
+                                href="https://github.com/cotebarrientos" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                            >
+                                <GrGithub className="mb-3 mt-4 me-2 ms-1 circle-icon"/>
+                            </a>
+                            <a 
+                                href="https://www.linkedin.com/in/mjbarrientosv/"
+                                className="" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                            >
+                                <GrLinkedin className="mb-3 mt-4 me-2 ms-1 circle-icon"/>
+                            </a>
+                    </Col>
+                    <Col lg={12} 
+                        className="text-light text-center">
+                        <p className="font-exo">
+                            © 2021-2022, Designed and Built by Maria Barrientos
+                        </p>
+                    </Col>
+                </Row>
+            </footer>
+        </Container>
+    )
+}
+
+export default Footer
