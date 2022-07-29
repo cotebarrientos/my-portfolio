8ee07391a2b1576cf95ff94e381f2447e072509f
cotebarrientos <cotebarrientos@gmail.com>
2022-07-29 19:02:31 +0100

    Updates

---
+++ precache-manifest.a5af884984d55092da9a487a0791b7be.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "5504ebe9e091c1ee650f",
+    "url": "/my-portfolio/static/css/main.42448b0d.chunk.css"
+  },
+  {
+    "revision": "5504ebe9e091c1ee650f",
+    "url": "/my-portfolio/static/js/main.5504ebe9.chunk.js"
+  },
+  {
+    "revision": "c3efb29e15f68a7d19c6",
+    "url": "/my-portfolio/static/css/1.8f8e7720.chunk.css"
+  },
+  {
+    "revision": "c3efb29e15f68a7d19c6",
+    "url": "/my-portfolio/static/js/1.c3efb29e.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "139804d9589c7c6d84d23e7d9314ea59",
+    "url": "/my-portfolio/static/media/html.139804d9.png"
+  },
+  {
+    "revision": "3fed7d64804b0c4df46c5359db9abf9c",
+    "url": "/my-portfolio/static/media/css.3fed7d64.png"
+  },
+  {
+    "revision": "bbd09f159c52bcea3e93142836812185",
+    "url": "/my-portfolio/static/media/javascript.bbd09f15.png"
+  },
+  {
+    "revision": "6798bb430085b6f899a7f17089989022",
+    "url": "/my-portfolio/static/media/heroku.6798bb43.png"
+  },
+  {
+    "revision": "964dc931e4a9f9792c92e7423a33e87a",
+    "url": "/my-portfolio/static/media/reactjs.964dc931.png"
+  },
+  {
+    "revision": "10ef412d513b343b63ffbc0062ce63f5",
+    "url": "/my-portfolio/static/media/firebase.10ef412d.png"
+  },
+  {
+    "revision": "8851df16b73533d422d96ab90e7f24f6",
+    "url": "/my-portfolio/static/media/jquery.8851df16.png"
+  },
+  {
+    "revision": "841a1c2e9a2f1d1c64572a862eb3010f",
+    "url": "/my-portfolio/static/media/python.841a1c2e.png"
+  },
+  {
+    "revision": "96d0c2bc277eb3227b5e5eae52e300ae",
+    "url": "/my-portfolio/static/media/django.96d0c2bc.png"
+  },
+  {
+    "revision": "5041fe93b870d22ea35bb782104da302",
+    "url": "/my-portfolio/static/media/flask.5041fe93.png"
+  },
+  {
+    "revision": "37eb37f71ca44c9e88efc5ca601846cc",
+    "url": "/my-portfolio/static/media/material_ui.37eb37f7.png"
+  },
+  {
+    "revision": "38f2a71b44a72e143da13c0a6a9da6d1",
+    "url": "/my-portfolio/static/media/mongodb.38f2a71b.png"
+  },
+  {
+    "revision": "b5c3c3f788731f855e8e0c74153a31e3",
+    "url": "/my-portfolio/static/media/my_sql.b5c3c3f7.png"
+  },
+  {
+    "revision": "d34b32d19be2c6d9275b26e067af4b2e",
+    "url": "/my-portfolio/static/media/reduxpic.d34b32d1.png"
+  },
+  {
+    "revision": "3a43d07e3dae281274507ac9bbc5e1c9",
+    "url": "/my-portfolio/static/media/github_pic.3a43d07e.jpg"
+  },
+  {
+    "revision": "ab851ebe9a9ef8f94b801d6d5f7bfe0e",
+    "url": "/my-portfolio/static/media/my_pic.ab851ebe.jpg"
+  },
+  {
+    "revision": "b4390dcac4f1b184442337892775a499",
+    "url": "/my-portfolio/static/media/mainCoverImg.b4390dca.jpg"
+  },
+  {
+    "revision": "84c2b72f16708dc736ea5464cb3f23dd",
+    "url": "/my-portfolio/static/media/coronavirus_report_website.84c2b72f.png"
+  },
+  {
+    "revision": "45ccfc560dc855b225b8f674fe0e9533",
+    "url": "/my-portfolio/static/media/your_argentine_shop.45ccfc56.jpg"
+  },
+  {
+    "revision": "a3319c74682a7ea5fb3f72f40e8ea8fe",
+    "url": "/my-portfolio/static/media/MyPokeApp.a3319c74.png"
+  },
+  {
+    "revision": "826156444e6896fe266f2384a2494f45",
+    "url": "/my-portfolio/static/media/fitness-heroes.82615644.png"
+  },
+  {
+    "revision": "072147738680f719bbeebe98a680a59e",
+    "url": "/my-portfolio/static/media/my-calculator.07214773.png"
+  },
+  {
+    "revision": "65783e0398f1884ddda6ee180671136a",
+    "url": "/my-portfolio/static/media/healhy_pet_website.65783e03.png"
+  },
+  {
+    "revision": "d207932d04569a1179c19639dffb1c5d",
+    "url": "/my-portfolio/static/media/my_veggie_food_website.d207932d.png"
+  },
+  {
+    "revision": "ea3f4c5a6819b10227d309c111f64a20",
+    "url": "/my-portfolio/static/media/aws.ea3f4c5a.png"
+  },
+  {
+    "revision": "dddb494722ecebdc81092587c1d17cce",
+    "url": "/my-portfolio/static/media/crud_simple_app.dddb4947.png"
+  },
+  {
+    "revision": "1f30d768a372aed3c14ef646229cf133",
+    "url": "/my-portfolio/static/media/weather_app.1f30d768.png"
+  },
+  {
+    "revision": "e80fb982ce39b17c8af2d9b0bf5b92c5",
+    "url": "/my-portfolio/static/media/storybookpic.e80fb982.png"
+  },
+  {
+    "revision": "2c183ef5ab36aba14dbef283f66d6790",
+    "url": "/my-portfolio/static/media/rock-paper-scissors.2c183ef5.png"
+  },
+  {
+    "revision": "23e307f77ea987bffd904aafe494cfbe",
+    "url": "/my-portfolio/static/media/contact.23e307f7.jpg"
+  },
+  {
+    "revision": "4b370762df4fc66706eba71552cc1d03",
+    "url": "/my-portfolio/index.html"
+  }
+];
